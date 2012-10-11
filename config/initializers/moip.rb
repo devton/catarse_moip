a487dc8d66af59011ab8216eaa50670e3867d718
Diogo Biazus <diogob@gmail.com>
2012-10-11 17:16:16 -0300

    added initializer with moip configuration

--- config/initializers/moip.rb
+++ config/initializers/moip.rb
@@ -1,8 +1,8 @@
-#Moip::Config.setup do |config|
-  #if Configuration[:moip_uri]
-    #config.uri = ::Configuration[:moip_uri]
-  #end
+::Moip::Config.setup do |config|
+  if ::Configuration[:moip_uri]
+    config.uri = ::Configuration[:moip_uri]
+  end
 
-  #config.token = Configuration[:moip_token] or ''
-  #config.key = Configuration[:moip_key] or ''
-#end
+  config.token = ::Configuration[:moip_token] or ''
+  config.key = ::Configuration[:moip_key] or ''
+end
