a62811642324266aa6e2e675a40e915b60d679d2
Diogo Biazus <diogob@gmail.com>
2012-10-19 21:33:57 -0300

    now we include the @moip.get_token response in the the POST get_moip_token response

--- app/controllers/catarse_moip/payment/moip_controller.rb
+++ app/controllers/catarse_moip/payment/moip_controller.rb
@@ -68,7 +68,7 @@ module CatarseMoip::Payment
         @backer.update_column :payment_token, response[:token]
       end
 
-      render json: { moip: @moip, widget_tag: @moip.widget_tag('checkoutSuccessful', 'checkoutFailure'), javascript_tag: @moip.javascript_tag }
+      render json: { get_token_response: response, moip: @moip, widget_tag: @moip.widget_tag('checkoutSuccessful', 'checkoutFailure'), javascript_tag: @moip.javascript_tag }
     end
   end
 end
--- spec/controllers/catarse_moip/payment/moip_controller_spec.rb
+++ spec/controllers/catarse_moip/payment/moip_controller_spec.rb
@@ -1,4 +1,22 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe CatarseMoip::Payment::MoipController do
+  subject{ response }
+  describe "POST get_moip_token" do
+    let(:get_token_response){{:status=>:fail, :code=>"171", :message=>"TelefoneFixo do endereço deverá ser enviado obrigatorio", :id=>"201210192052439150000024698931"}}
+
+    before do
+      @backer = Factory(:backer, :confirmed => false)
+      controller.stub(:current_user).and_return(@backer.user)
+      ::MoipTransparente::Checkout.any_instance.stub(:get_token).and_return(get_token_response)
+      ::MoipTransparente::Checkout.any_instance.stub(:moip_widget_tag).and_return('<div>')
+      ::MoipTransparente::Checkout.any_instance.stub(:moip_javascript_tag).and_return('<script>')
+      ::MoipTransparente::Checkout.any_instance.stub(:as_json).and_return('{}')
+      post :get_moip_token, :id => @backer.id, :use_route => 'catarse_moip'
+    end
+
+    its(:status){ should == 200 }
+    its(:body){ should == "{\"get_token_response\":{\"status\":\"fail\",\"code\":\"171\",\"message\":\"TelefoneFixo do endere\\u00e7o dever\\u00e1 ser enviado obrigatorio\",\"id\":\"201210192052439150000024698931\"},\"moip\":\"{}\",\"widget_tag\":\"<div id='MoipWidget'\\n          data-token=''\\n          callback-method-success='checkoutSuccessful' \\n          callback-method-error='checkoutFailure'>\\n    </div>\",\"javascript_tag\":\"<script type='text/javascript' src='https://www.moip.com.br/transparente/MoipWidget-v2.js' charset='ISO-8859-1'></script>\"}" }
+  end
 end
