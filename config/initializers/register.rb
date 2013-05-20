96e1b4788dab4aa32728f97b685864c082e24f75
Diogo Biazus <diogob@gmail.com>
2013-05-20 09:59:57 -0300

    finished removing payment namespace

--- config/initializers/register.rb
+++ config/initializers/register.rb
@@ -1,5 +1,5 @@
 begin
-  PaymentEngines.register({name: 'moip', review_path: ->(backer){ CatarseMoip::Engine.routes.url_helpers.review_payment_moip_path(backer) }, locale: 'pt'})
+  PaymentEngines.register({name: 'moip', review_path: ->(backer){ CatarseMoip::Engine.routes.url_helpers.review_moip_path(backer) }, locale: 'pt'})
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 CatarseMoip::Engine.routes.draw do
-  namespace :payment do
-    resources :moip, only: [] do
-      collection do
-        post 'notifications' => "moip#create_notification"
-        get 'js'
-      end
-      member do
-        match :moip_response
-        match :review
-        match :get_moip_token
-      end
+  resources :moip, only: [], path: 'payment/moip' do
+    collection do
+      post 'notifications' => "moip#create_notification"
+      get 'js'
+    end
+    member do
+      match :moip_response
+      match :review
+      match :get_moip_token
     end
   end
 end
