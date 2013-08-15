08402bfeeee2ef325bc342bc582217ea75858dcc
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-15 13:41:00 -0500

    Enable escape_html_entities_in_json

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,5 +19,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
   end
 end
