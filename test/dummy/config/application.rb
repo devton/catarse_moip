e154e1642c4c530dbbcfb71c32b8251ff2a8657b
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-15 13:57:11 -0500

    Revert "Enable escape_html_entities_in_json"
    This reverts commit 08402bfeeee2ef325bc342bc582217ea75858dcc.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,8 +19,5 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
   end
 end
