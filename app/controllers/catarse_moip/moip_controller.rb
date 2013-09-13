77c18aab2afe0a91e183b8ec26052d01c4326d04
Antônio Roberto Silva <forevertonny@gmail.com>
2013-09-13 17:22:45 -0300

    remove else because moip send last notification sometimes with status finished and we dont need to move backer to pending

--- app/controllers/catarse_moip/moip_controller.rb
+++ app/controllers/catarse_moip/moip_controller.rb
@@ -122,8 +122,6 @@ module CatarseMoip
             backer.refund! unless backer.refunded?
           when TransactionStatus::CANCELED
             backer.cancel! unless backer.canceled?
-          else
-            backer.waiting! if backer.pending?
           end
         end
       end
