08f978efbca03d23854403dfbb41257c98ac2532
Antônio Roberto Silva <forevertonny@gmail.com>
2014-03-18 14:07:46 -0300

    we dont need to use payment engine interface

--- lib/catarse_moip/payment_engine.rb
+++ lib/catarse_moip/payment_engine.rb
@@ -1,21 +1,25 @@
-begin
-  module CatarseMoip
-    class PaymentEngine < PaymentEngines::Interface
+module CatarseMoip
+  class PaymentEngine
 
-      def name
-        'MoIP'
-      end
-
-      def review_path contribution
-        CatarseMoip::Engine.routes.url_helpers.review_moip_path(contribution)
-      end
-
-      def locale
-        'pt'
-      end
-
+    def name
+      'MoIP'
     end
+
+    def review_path contribution
+      CatarseMoip::Engine.routes.url_helpers.review_moip_path(contribution)
+    end
+
+    def locale
+      'pt'
+    end
+
+    def can_do_refund?
+      false
+    end
+
+    def direct_refund
+      false
+    end
+
   end
-rescue Exception => e
-  puts "Error while use payment engine interface: #{e}"
 end
